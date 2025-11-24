8bed8e27e56ecb0655e5ab850951acf8392aa9f5
reniejoshi <renie_joshi@yahoo.com>
2025-11-24 15:35:45 -0800

    Add sleep method, user character selection method, and method to handle game play

--- Game.java
+++ Game.java
@@ -1,3 +1,39 @@
+import java.util.Scanner;
+
 public class Game {
-    
+    private Character character;
+    private CharacterData characterData = new CharacterData();
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.playGame();
+    }
+
+    private void playGame() {
+        selectCharacter();
+    }
+
+    private void selectCharacter() {
+        System.out.println("Select character: ");
+        characterData.characters.forEach(characterDataCharacter -> System.out.println(characterDataCharacter.getName()));
+
+        String name = scanner.nextLine();
+        switch(name) {
+            case "Boxer" -> {
+                character = characterData.characters.get(0);
+            }
+        }
+
+        System.out.println("Welcome to Manor Farm, comrade " + character.getName() + "!");
+    }
+
+    private void sleep(int milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
