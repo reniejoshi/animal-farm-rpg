644f8d9da445bfee1de3d31056194d0eb9020039
reniejoshi <renie_joshi@yahoo.com>
2025-11-24 15:22:15 -0800

    Create Character class
    This commit adds variables, constructor, and getters to the Character class.

--- Character.java
+++ Character.java
@@ -1,3 +1,34 @@
 public class Character {
-    
+    public String name;
+    public SocialStatus socialStatus;
+    public int laborStrength;
+    public int educationLevel;
+
+    public enum SocialStatus {
+        CORRUPT_LEADER,
+        WORKING_CLASS
+    }
+
+    public Character(String name, SocialStatus socialStatus, int laborStrength, int educationLevel) {
+        this.name = name;
+        this.socialStatus = socialStatus;
+        this.laborStrength = laborStrength;
+        this.educationLevel = educationLevel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public SocialStatus getSocialStatus() {
+        return socialStatus;
+    }
+
+    public int getLaborStrength() {
+        return laborStrength;
+    }
+
+    public int getEducationLevel() {
+        return educationLevel;
+    }
 }
